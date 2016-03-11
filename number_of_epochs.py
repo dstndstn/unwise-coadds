457f0c35b020e72681d919f5a785279b8dc4c3b5
Aaron Meisner <ameisner@fas.harvard.edu>
2016-03-11 00:21:51 -0800

    added some plotting code

--- number_of_epochs.py
+++ number_of_epochs.py
@@ -2,21 +2,65 @@ import fitsio
 import numpy as np
 from unwise_coadd import get_wise_frames
 from unwise_utils import get_epoch_breaks
+from astrometry.util.fits import fits_table
+import matplotlib.pyplot as plt
 
-def process_one_tile(racen, deccen, band):
+def process_one_tile(racen, deccen, band, subdivide=False):
     # call get_wise_frames
     frames = get_wise_frames(racen, deccen, band)
     # call get_epoch_breaks using the MJD values where qual_frame is good
 
-    ebreaks = get_epoch_breaks(frames[frames.qual_frame > 0].mjd)
+    ebreaks = get_epoch_breaks(frames[frames.qual_frame > 0].mjd, 
+                               subdivide=subdivide)
+
+    plot = False
+    if plot:
+        nmjd = len(frames[frames.qual_frame > 0].mjd)
+        plt.subplot(2,1,1)
+        plt.scatter(frames[frames.qual_frame > 0].mjd, np.zeros(nmjd), c='k', edgecolor='none')
+        for i in range(len(ebreaks)):
+            plt.plot([ebreaks[i], ebreaks[i]], [-0.5,0.5], c='r')
+
+        plt.xlim((55200, 55600))
+        plt.ylim((-0.5, 0.5))
+        plt.yticks([])
+
+        plt.title('number of epoch breaks = ' + str(len(ebreaks)))
+
+        plt.subplot(2,1,2)
+        plt.scatter(frames[frames.qual_frame > 0].mjd, np.zeros(nmjd), c='k', edgecolor='none')
+        for i in range(len(ebreaks)):
+            plt.plot([ebreaks[i], ebreaks[i]], [-0.5,0.5], c='r')
+
+        plt.xlim((56625, 57025))
+        plt.ylim((-0.5, 0.5))
+        plt.yticks([])
+        plt.xlabel('MJD')
+        plt.show()
+
+        plt.scatter(frames[frames.qual_frame > 0].mjd, np.zeros(nmjd), c='k', edgecolor='none')
+        for i in range(len(ebreaks)):
+            plt.plot([ebreaks[i], ebreaks[i]], [-0.5,0.5], c='r')
+
+        plt.xlim((55200, 57025))
+        plt.ylim((-0.5, 0.5))
+        plt.yticks([])
+        plt.xlabel('MJD')
+        plt.title('number of epoch breaks = ' + str(len(ebreaks)))
+        plt.show()
 
     # return the number of epochs
     n_epoch = len(ebreaks) + 1
     return n_epoch
 
-def process_many_tiles(band, indstart, nproc):
+def process_many_tiles(band, indstart, nproc, subdivide=False, fname='/n/fink1/ameisner/unwise/allsky-atlas.fits'):
+
+    # fname is meant to be the name of the -atlas.fits file containing the
+    # list of tiles that are of interest ... it defaults to the file with
+    # all 18240 tiles
+
     # read in atlas file
-    tab = fitsio.read('/n/fink1/ameisner/unwise/allsky-atlas.fits')
+    tab = fitsio.read(fname)
 
     ntile = len(tab)
     # determine indend
@@ -29,9 +73,9 @@ def process_many_tiles(band, indstart, nproc):
     # loop over tile id's
     for i in range(len(tab)):
     # call process_one_tile
-        n_epoch = process_one_tile(tab['ra'][i], tab['dec'][i], band)
+        n_epoch = process_one_tile(tab['RA'][i], tab['DEC'][i], band, subdivide=subdivide)
         # print tile name and number of epochs so that they can be included in log file
-        print tab['coadd_id'][i], n_epoch
+        print tab['COADD_ID'][i], n_epoch
         n_epoch_list[i] = n_epoch
     
     # write out the per-tile number of epochs
